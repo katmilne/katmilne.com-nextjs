315a831859464d3b18a837ba8c48e399332708f9
katmilne <katmilne39@gmail.com>
2024-02-04 22:36:03 +0000

    added some markdown on cv page

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,11 +1,15 @@
 import Navigation from '../components/Layout/Navigation';
 import Footer from '../components/Layout/Footer';
+import CVContent from '../components/Layout/CV';
 
 const CV = () => {
   
+    const markdownCV = `...`;
+
     return (
         <>
             <Navigation />
+            <CVContent markdownContent={markdownCV} />
             <Footer />
         </>
         
