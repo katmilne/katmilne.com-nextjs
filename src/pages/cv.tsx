0a940257b34a92315c9fb85e832b7c3c5c212a72
Kat Milne <93844865+katmilne@users.noreply.github.com>
2024-02-04 22:45:15 +0000

    Update cv.tsx

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,15 +1,12 @@
 import Navigation from '../components/Layout/Navigation';
 import Footer from '../components/Layout/Footer';
-import CVContent from '../components/Layout/CV';
+
 
 const CV = () => {
-  
-    const markdownCV = `...`;
 
     return (
         <>
             <Navigation />
-            <CVContent markdownContent={markdownCV} />
             <Footer />
         </>
         
