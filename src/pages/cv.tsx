a17fde81d4c96f9e486a906ffeb8d460387b8e6d
katmilne <katmilne39@gmail.com>
2024-02-07 15:22:09 +0000

    added phone call link to cv page

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -30,11 +30,11 @@ const CV = () => {
                 </div>
 
                 {/* Links */}
-                <div className='flex justify-center md:m-2 w-full '>
-                        <div className="flex mdp-2 md:mx-2 m-2 ml-8 ">
+                <div className='flex justify-center md:m-2 w-full '> 
+                        <Link className="flex mdp-2 md:mx-2 m-2 ml-8" href="tel: 07718939345">
                             <Image src="/img/phone.svg" alt="Phone" width={32} height={32} className="md:mx-1 "/>
                             <h4 className='pt-1 mr-2 hidden md:block md:text-sm lg:text-base'>(+44)7718939345</h4>
-                        </div>
+                        </Link>
                         
                         <Link className="flex mdp-2 md:mx-2 m-2" href="mailto: katmilne39@gmail.com">
                             <Image src="/img/mail.svg" alt="Mail" width={35} height={35} className="md:mx-1"/>
