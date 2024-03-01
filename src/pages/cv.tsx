0cab050476aa07a3dde25faf0c337ebaf93c124b
katmilne <katmilne39@gmail.com>
2024-03-01 17:41:24 +0000

    updated CV to add projects

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -30,11 +30,7 @@ const CV = () => {
                 </div>
 
                 {/* Links */}
-                <div className='flex justify-center md:m-2 w-full '> 
-                        {/* <Link className="flex mdp-2 md:mx-2 m-2 ml-8" href="tel: 07718939345">
-                            <Image src="/img/phone.svg" alt="Phone" width={32} height={32} className="md:mx-1 "/>
-                            <h4 className='pt-1 mr-2 hidden md:block md:text-sm lg:text-base'>(+44)7718939345</h4>
-                        </Link> */}
+                <div className='flex justify-center md:m-2 w-full '>
                         
                         <Link className="flex mdp-2 md:mx-2 m-2" href="mailto: katmilne39@gmail.com">
                             <Image src="/img/mail.svg" alt="Mail" width={35} height={35} className="md:mx-1"/>
@@ -54,28 +50,68 @@ const CV = () => {
                 <section className='font-[Inter] md:mt-2 '>
                     <h2 className='md:text-4xl text-2xl font-bold font-[sono] md:my-6 my-2'>About Me</h2>
                     <div className='lg:flex lg:flex-grow lg:my-2  '>
-                        <div className='font-bold whitespace-nowrap md:mr-6'>Technical  Skills</div>
-                            <div className='grid grid-cols-3 md:grid-cols-6 gap-x-1 gap-y-1 xl:flex xl:justify-between w-full text-rose-300 font-bold mt-2 md:mt-0'>
+                        <div className='font-bold whitespace-nowrap md:mr-6'>Technical Skills</div>
+                            <div className='grid grid-cols-3 text-rose-300 font-bold w-full mt-2
+                                            md:grid-cols-6 md:mt-0
+                                            xl:grid-cols-7'>
                                 <div className='flex justify-center'>JavaScript</div>
+                                <div className='flex justify-center'>Java</div>
+                                <div className='flex justify-center'>Python</div>
                                 <div className='flex justify-center'>HTML</div>
                                 <div className='flex justify-center'>CSS</div>
-                                <div className='flex justify-center'>Python</div>
-                                <div className='flex justify-center'>Java</div>
+                                <div className='flex justify-center'>React</div>
+                                <div className='flex justify-center'>NextJS</div>
+                                <div className='flex justify-center'>React Native</div>
                                 <div className='flex justify-center'>Git</div>
                                 <div className='flex justify-center'>SQL</div>
-                                <div className='flex justify-center'>Unix</div>
                                 <div className='flex justify-center'>Linux</div>
-                                <div className='flex justify-center'>Next.js</div>
-                                <div className='flex justify-center'>Node</div>
-                                <div className='flex justify-center'>Tailwind</div>
-                                
+                                <div className='flex justify-center'>Figma</div>
+                                <div className='flex justify-center'>TailwindCSS</div>
+                                <div className='flex justify-center'>CI/CD</div>
                             </div>
                     </div>
-                    <p className='md:py-2 my-4 md:text-md text-sm'>I’m a creative and detail‑oriented individual with a deep passion for infusing artistry into coding to craft innovative and
+                    <p className='md:py-2 mt-4 md:text-md text-sm'>I’m a creative and detail‑oriented individual with a deep passion for infusing artistry into coding to craft innovative and
                         user‑friendly software. My goal is to harness the potential of software to enhance and enrich people’s daily experiences.
                         I’m dedicated to delivering high‑quality work and thrive in collaborative environments, where I love bouncing around
                         ideas and learning from my fellow team members.
                     </p>
+                </section>
+
+                {/* Personal Projectsparagraph */}
+                <section className='font-[inter] w-full'>
+                    <h2 className='md:text-4xl text-3xl font-bold font-[sono] md:my-6'>Personal Projects</h2>
+
+                    {/* My Website */}
+                    <div className='flex my-2 justify-between'>
+                        <div className='font-bold text-xl'>Personal Website</div>
+                        <div className='font-light  text-rose-300'>Birmingham, UK</div>
+                    </div>
+                    <div className='flex my-2 justify-between'>
+                        <div className='font-light text-sm '>KATMILNE.COM</div>
+                        <div className='font-light text-sm text-rose-300'>Feb. 2024</div>
+                    </div>
+                    <ul className='list-disc font-[Inter]'>
+                        <li className='md:text-md text-sm'>Developed a personal website with <span className='text-rose-300 font-bold'>Next.js</span> and <span className='text-rose-300 font-bold'>Tailwind CSS</span>, utlising <span className='text-rose-300 font-bold'>page-based routing.</span></li>
+                        <li className='md:text-md text-sm'>Ensured <span className='text-rose-300 font-bold'>responsiveness</span> across all screen sizes through <span className='text-rose-300 font-bold'>responsive design principles</span> and rigorous <span className='text-rose-300 font-bold'>testing.</span></li>
+                        <li className='md:text-md text-sm'><span className='text-rose-300 font-bold'>Deployed</span> the website on Vercel for efficient hosting and <span className='text-rose-300 font-bold'>continuous deployment</span>.
+                        </li>
+                    </ul>
+                    
+                    {/* My Study App */}
+                    <div className='flex my-2 mt-6 justify-between'>
+                        <div className='font-bold text-xl'>Blobodoro: Study App (IOS/Android)</div>
+                        <div className='font-light  text-rose-300'>Birmingham, UK</div>
+                    </div>
+                    <div className='flex my-2 justify-between'>
+                        <div className='font-light text-sm '>BLOBODORO.COM</div>
+                        <div className='font-light text-sm text-rose-300'>Dec. 2023 - Present</div>
+                    </div>
+                    <ul className='list-disc font-[Inter]'>
+                        <li className='md:text-md text-sm'>Employed <span className='text-rose-300 font-bold'>Git</span> along with <span className='text-rose-300 font-bold'>Kanban</span> methodology to efficiently track feature development and project progress.</li>
+                        <li className='md:text-md text-sm'>Designed Blobodoro's user interface in <span className='text-rose-300 font-bold'>Figma</span>, employing reusable components for easy iteration and editing of designs.</li>
+                        <li className='md:text-md text-sm'>Integrated <span className='text-rose-300 font-bold'>Clerk</span>, a user-friendly <span className='text-rose-300 font-bold'>authentication</span> and user management solution, into Blobodoro using <span className='text-rose-300 font-bold'>React Native</span>, enabling swift implementation of <span className='text-rose-300 font-bold'>sign-up</span>, <span className='text-rose-300 font-bold'>log-in</span>, and <span className='text-rose-300 font-bold'>profile management</span> functionalities within the app.</li>
+                        <li className='md:text-md text-sm'>Structured Blobodoro app with a three-page layout in <span className='text-rose-300 font-bold'>React Native</span> with the use of <span className='text-rose-300 font-bold'>Expo router</span>: Aquarium for storing gained fish, Stats page for goal tracking and habit analysis, and a Shop interface for purchasing rewards.</li>
+                    </ul>
                 </section>
 
                 {/* Education paragraph */}
