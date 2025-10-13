b1c22f3c18ff3977156960fdbe752927f6302908
Your Name <you@example.com>
2025-10-13 12:08:55 -0700

    Add poppy logo as website favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Pangolin&family=VT323&family=Sono&display=swap"
             rel="stylesheet"
           />
-            {/* Global styles are imported in _app.tsx, so this is not needed */}
+          {/* Favicon */}
+          <link rel="icon" type="image/png" href="/img/poppy3.png" />
+          <link rel="shortcut icon" type="image/png" href="/img/poppy3.png" />
+          <link rel="apple-touch-icon" href="/img/poppy3.png" />
+          {/* Global styles are imported in _app.tsx, so this is not needed */}
         </Head>
         <body>
           <Main />
